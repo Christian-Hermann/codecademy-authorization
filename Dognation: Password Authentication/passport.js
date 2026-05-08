2635b3bd1fa8fb0c7e2caf53caa016acf818e3b9
Your NameChristian <christianhermanndev@gmail.com>
2026-05-08 14:12:22 -0500

    Questions 1 - 14

--- Dognation: Password Authentication/passport.js
+++ Dognation: Password Authentication/passport.js
@@ -3,8 +3,25 @@ const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 const helper = require("../helpers/helper");
 
-// Set up the Passport strategy:
+// Session Config
+app.use(session({...}));
+
+// Passport Config
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Serialize a user
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
 // Deserialize a user
+passport.deserializeUser((id, done) => {
+  helper.findById(id, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+
+    return done(null, user);
+  });
+});
